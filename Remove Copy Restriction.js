4e0938ea4645e6623fa8f438d4262cf13f8defa7
Lucas04 <117806313+Lucas04-nhr@users.noreply.github.com>
2024-02-26 12:31:25 +0800

    Update user authentication logic

---
+++ Remove Copy Restriction.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Remove Copy Restriction
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove copy restriction on web pages
+// @author       You
+// @match        *://*/*
+// @updateURL    https://raw.githubusercontent.com/Lucas04-nhr/TamperMonkeyUserScript/main/Remove%20Copy%20Restriction.js
+// @downloadURL  https://raw.githubusercontent.com/Lucas04-nhr/TamperMonkeyUserScript/main/Remove%20Copy%20Restriction.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.body.style.userSelect = 'auto';
+    document.body.style.webkitUserSelect = 'auto';
+    document.body.style.mozUserSelect = 'auto';
+    document.body.style.msUserSelect = 'auto';
+    document.oncontextmenu = null;
+    document.ondragstart = null;
+    document.onselectstart = null;
+})();
