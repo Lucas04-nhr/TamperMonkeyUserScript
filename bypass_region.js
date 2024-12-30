d4571df252f55e8164cfe0979d3ac931109f9d99
Lucas04-nhr <lucas04@foxmail.com>
2024-12-30 16:06:25 +0800

    Update bypass_region.js to version 0.4.5 and add toggle functionality for bypass

--- bypass_region.js
+++ bypass_region.js
@@ -4,13 +4,13 @@
 // @name:zh-CN   绕过 GitLab 地区限制
 // @name:zh-TW   繞過 GitLab 地區限制
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
+// @version      0.4.5
 // @description:en  Bypass the GitLab Region Restriction by modifying the navigator.language field
 // @description:zh-CN  通过修改 navigator.language 字段绕过 GitLab 地区限制
 // @description:zh-TW  通過修改 navigator.language 字段繞過 GitLab 地區限制
 // @author       Lucas
 // @match        https://*.gitlab.com/*
-// @grant        none
+// @grant        GM_registerMenuCommand
 // @license      GPLv3
 // @downloadURL https://update.greasyfork.org/scripts/522277/Bypass%20the%20GitLab%20Region%20Restriction.user.js
 // @updateURL https://update.greasyfork.org/scripts/522277/Bypass%20the%20GitLab%20Region%20Restriction.meta.js
@@ -21,15 +21,27 @@
 
   const newLanguage = 'en-US';
 
+  let isEnabled = true;
+
+  function toggleBypass() {
+      isEnabled = !isEnabled;
+      alert(`Bypass is now ${isEnabled ? 'enabled' : 'disabled'}`);
+  }
+
+  GM_registerMenuCommand('Toggle Bypass', toggleBypass);
+
+  const originalLanguage = navigator.language;
+  const originalLanguages = navigator.languages;
+
   Object.defineProperty(navigator, 'language', {
       get: function() {
-          return newLanguage;
+          return isEnabled ? newLanguage : originalLanguage;
       }
   });
 
   Object.defineProperty(navigator, 'languages', {
       get: function() {
-          return [newLanguage];
+          return isEnabled ? [newLanguage] : originalLanguages;
       }
   });
 })();
